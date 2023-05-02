36f58a81599162fc078158d5148f71e00e722941
GXtal <zazaza00133@gmail.com>
2023-05-02 13:05:43 +0300

    Fixes
    FIxed naming in function parametrs
    Fixed indentations
    Now in get_brands_with_products_count using LEFT JOIN to return all brands, even without products

--- query_functions.sql
+++ query_functions.sql
@@ -1,8 +1,8 @@
---         prod by brand
+-- All products by brand
 
-CREATE OR REPLACE FUNCTION get_products_by_brand(wanted_brand INTEGER)
+CREATE OR REPLACE FUNCTION get_products_by_brand(brand_id INTEGER)
 RETURNS TABLE (
-    product_id INTEGER,
+	product_id INTEGER,
     product_name VARCHAR(45),
     product_price MONEY,
     product_description VARCHAR(200),
@@ -16,34 +16,34 @@ BEGIN
     FROM products p
 	JOIN categories c ON p.category_id=c.id
 	JOIN brands b ON p.brand_id=b.id
-    WHERE brand_id = wanted_brand;
+    WHERE p.brand_id = get_products_by_brand.brand_id;
 END;
 $$ LANGUAGE plpgsql;
 
 
--- Brands by the number of products
+-- All brands with count of their products sorted by the number of products
 
-CREATE OR REPLACE FUNCTION get_brand_products()
-RETURNS TABLE (b
-	rand_id integer,
+CREATE OR REPLACE FUNCTION get_brands_with_products_count()
+RETURNS TABLE (
+	brand_id integer,
 	brand_name VARCHAR(45),
-	num_products BIGINT
+	products_count BIGINT
 ) AS $$
 BEGIN
-  RETURN QUERY
-  SELECT b.id, b.name, COUNT(p.id) AS num_products
-  FROM brands b
-  JOIN products p ON b.id = p.brand_id
-  GROUP BY b.id
-  ORDER BY num_products DESC;
+	RETURN QUERY
+	SELECT b.id, b.name, COUNT(p.id) AS num_products
+	FROM brands b
+	LEFT JOIN products p ON b.id = p.brand_id
+	GROUP BY b.id
+	ORDER BY num_products DESC;
 END;
 $$
 LANGUAGE plpgsql;
 
 
--- Products by category and section?
+-- Products by category and section
 
-CREATE OR REPLACE FUNCTION get_products_by_category_and_section(category_search_id integer,section_search_id integer)
+CREATE OR REPLACE FUNCTION get_products_by_category_and_section(category_id integer,section_id integer)
 RETURNS TABLE (
     product_id INT,
     product_name VARCHAR(45),
@@ -61,20 +61,21 @@ BEGIN
     JOIN brands b ON p.brand_id = b.id
     JOIN categories_sections cs ON c.id = cs.category_id
     JOIN sections s ON cs.section_id = s.id
-    WHERE c.id = category_search_id AND s.id = section_search_id;
+    WHERE c.id = get_products_by_category_and_section.category_id
+	AND s.id = get_products_by_category_and_section.section_id;
 END;
 $$ LANGUAGE plpgsql;
 
 
--- completed orders for product
+-- Completed orders for product
 
-CREATE OR REPLACE FUNCTION get_completed_orders_for_product(p_product_id INT)
+CREATE OR REPLACE FUNCTION get_completed_orders_by_product(product_id INT)
 RETURNS TABLE (
-  order_id INT,
-  user_id INT,
-  total_order_cost MONEY,
-  count INT,
-  complete_time TIMESTAMP
+	order_id INT,
+	user_id INT,
+	total_order_cost MONEY,
+	product_count INT,
+	complete_time TIMESTAMP
 ) AS $$
 BEGIN
 	RETURN QUERY
@@ -82,8 +83,7 @@ BEGIN
     FROM orders o
     JOIN order_histories oh ON oh.order_id = o.id
     JOIN orders_products op ON op.order_id = o.id
-    WHERE oh.status_id = 3
-      AND op.product_id = p_product_id
+    WHERE oh.status_id = 3 AND op.product_id = get_completed_orders_by_product.product_id
     ORDER BY oh.set_time DESC;
 END;
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;	
--- triggers.sql
+++ triggers.sql
@@ -1,12 +1,12 @@
 CREATE OR REPLACE FUNCTION update_order_history_set_time()
-  RETURNS TRIGGER AS $$
+RETURNS TRIGGER AS $$
 BEGIN
-  NEW.set_time := NOW();
-  RETURN NEW;
+	NEW.set_time := NOW();
+	RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER tr_update_order_history_set_time
-  BEFORE INSERT ON order_histories
-  FOR EACH ROW
-  EXECUTE FUNCTION update_order_history_set_time();
+	BEFORE INSERT ON order_histories
+	FOR EACH ROW
+	EXECUTE FUNCTION update_order_history_set_time();

74bbd9eb5b60280dfebdf2d6cf85db1fa031159f
GXtal <zazaza00133@gmail.com>
2023-05-02 19:30:56 +0300

    last formating

--- triggers.sql
+++ triggers.sql
@@ -1,12 +1,8 @@
-CREATE OR REPLACE FUNCTION update_order_history_set_time()
-RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION update_order_history_set_time() RETURNS TRIGGER AS $$ 
 BEGIN
 	NEW.set_time := NOW();
-	RETURN NEW;
+RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
-
-CREATE TRIGGER tr_update_order_history_set_time
-	BEFORE INSERT ON order_histories
-	FOR EACH ROW
-	EXECUTE FUNCTION update_order_history_set_time();
+CREATE TRIGGER tr_update_order_history_set_time BEFORE
+INSERT ON order_histories FOR EACH ROW EXECUTE FUNCTION update_order_history_set_time();

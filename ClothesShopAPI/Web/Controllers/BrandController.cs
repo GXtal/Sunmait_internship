4b4c028a5382bc3a4c4a3e550293ffe5b72d7484
GXtal <zazaza00133@gmail.com>
2023-05-26 16:47:19 +0300

    Small fixes
    Changed names of controller methods, also changed names of variables into 'result'

--- ClothesShopAPI/Web/Controllers/BrandController.cs
+++ ClothesShopAPI/Web/Controllers/BrandController.cs
@@ -17,22 +17,22 @@ namespace Web.Controllers
 
         // GET: api/Brands
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> GetBrands()
         {
             var allBrands = await _brandRepository.GetBrands();
 
-            var allBrandsSimplified = new List<BrandViewModel>();
+            var result = new List<BrandViewModel>();
             foreach (var brand in allBrands)
             {
-                allBrandsSimplified.Add(new BrandViewModel { Id = brand.Id, Name = brand.Name });
+                result.Add(new BrandViewModel { Id = brand.Id, Name = brand.Name });
             }
 
-            return new OkObjectResult(allBrandsSimplified);
+            return new OkObjectResult(result);
         }
 
         // GET api/Brands/5
         [HttpGet("{id}")]
-        public async Task<IActionResult> Get(int id)
+        public async Task<IActionResult> GetBrandById(int id)
         {
             var brand = await _brandRepository.GetBrandById(id);
 
@@ -41,13 +41,13 @@ namespace Web.Controllers
                 return NotFound();
             }
 
-            var brandSimplified = new BrandViewModel() { Id = brand.Id, Name = brand.Name };
-            return new OkObjectResult(brandSimplified);
+            var result = new BrandViewModel() { Id = brand.Id, Name = brand.Name };
+            return new OkObjectResult(result);
         }
 
         // POST api/Brands
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] BrandInputModel newBrand)
+        public async Task<IActionResult> AddBrand([FromBody] BrandInputModel newBrand)
         {
             bool result = await _brandRepository.AddBrand(newBrand.Name);
 
@@ -61,7 +61,7 @@ namespace Web.Controllers
 
         // PUT api/Brands/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> Put(int id, [FromBody] BrandInputModel newBrand)
+        public async Task<IActionResult> UpdateBrand(int id, [FromBody] BrandInputModel newBrand)
         {
             bool result = await _brandRepository.UpdateBrand(id, newBrand.Name);
 
@@ -75,7 +75,7 @@ namespace Web.Controllers
 
         // DELETE api/Brands/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
+        public async Task<IActionResult> RemoveBrand(int id)
         {
             bool result = await _brandRepository.RemoveBrand(id);
 
